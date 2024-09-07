d06539e0525d85a47300db97876894122ef90938
Jayden-747 <jaydenhutchinson747@gmail.com>
2024-09-07 16:08:28 -0700

    fully updated and tested the name class

--- src/code/ca/bcit/comp2522/bank/Name.java
+++ src/code/ca/bcit/comp2522/bank/Name.java
@@ -1,36 +1,55 @@
 package ca.bcit.comp2522.bank;
+
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Locale;
+
 /**
- *  Name class that must not be null or blank, and cannot be more than 45 characters.
- *  This class contains methods that will return the initials, full name and reverse
- *  the name input.
+ * Name class
+ * must not be null or blank,
+ * cannot be more than 45 characters.
+ * <p>
+ * Contains methods that will return the initials, full name and reverse
+ * the name input.
  *
- * @author Richard Ho and Jayden
+ * @author Richard Ho and Jayden Hutchinson
  * @version 1.0
  */
 public class Name
 {
+    // instance variables
     private final String first;
     private final String last;
-    private static final int MAX_STRING_SIZE = 45;
+
+    // symbolic constants
+    private static final int MAX_NAME_LENGTH_CHARS = 45;
+    private static final int FIRST_CHAR = 0;
+    private static final int SECOND_CHAR = 1;
 
     /**
      * Constructor for the Name class.
-     * @param first a string that takes first name
-     * @param last a string that takes last name
+     *
+     * @param first String that takes first name
+     * @param last  String that takes last name
+     *
+     * @throws IllegalArgumentException first or last name is null, bank, or less than the maximum name length in
+     *                                  characters
      */
-    public Name(final String first, final String last) {
+    public Name(final String first, final String last)
+    {
+        // validate Inputs
         validateFirstName(first);
         validateLastName(last);
 
+        // assign Variables
         this.first = first;
         this.last = last;
     }
 
     private static void validateFirstName(final String first)
     {
-        if(first == null || first.isBlank() || first.length() > MAX_STRING_SIZE || first.toLowerCase().contains("admin"))
+        // if first is null, blank, or less than maximum length, throw exception
+        if (first == null || first.isBlank() || first.length() > MAX_NAME_LENGTH_CHARS || first.toLowerCase().contains("admin"))
         {
             throw new IllegalArgumentException("Invalid first name entry");
         }
@@ -38,7 +57,8 @@ public class Name
 
     private static void validateLastName(final String last)
     {
-        if(last == null || last.isBlank() || last.length() > MAX_STRING_SIZE || last.toLowerCase().contains("admin"))
+        // if last is null, blank, or less than maximum length, throw exception
+        if (last == null || last.isBlank() || last.length() > MAX_NAME_LENGTH_CHARS || last.toLowerCase().contains("admin"))
         {
             throw new IllegalArgumentException("Invalid last name entry");
         }
@@ -46,7 +66,8 @@ public class Name
 
     /**
      * Accessor for first name.
-     * @return first a String
+     *
+     * @return first name
      */
     public String getFirst()
     {
@@ -55,7 +76,8 @@ public class Name
 
     /**
      * Accessor for last name.
-     * @return last a String
+     *
+     * @return last name
      */
     public String getLast()
     {
@@ -63,53 +85,57 @@ public class Name
     }
 
     /**
-     * Method that converts the first and last name to their initials capitalized.
+     * Converts the first and last name to their initials capitalized.
+     *
      * @return initials a String
      */
     public String getInitials()
     {
+        // initials string builder initialization
         final StringBuilder initials;
-
         initials = new StringBuilder();
-        initials.append(first.toUpperCase().charAt(0));
-        initials.append(".");
-        initials.append(last.toUpperCase().charAt(0));
-        initials.append(".");
 
-        return initials.toString();
+        initials.append(first.toUpperCase().charAt(FIRST_CHAR));// append Capital First Initial
+        initials.append("."); // append period
+        initials.append(last.toUpperCase().charAt(FIRST_CHAR)); // append Capital Last Initial
+        initials.append("."); // append period
+
+        return initials.toString(); // return initials
     }
 
     /**
-     * Method that concatenates both the first and last name input.
+     * Concatenates both the first and last name input.
+     *
      * @return Full name concatenated
      */
     public String getFullName()
     {
+        // full name string builder initialization
         final StringBuilder fullName;
-
         fullName = new StringBuilder();
-        fullName.append(first);
-        fullName.append(" ");
-        fullName.append(last);
 
-        return fullName.toString();
+        fullName.append(first.substring(FIRST_CHAR, SECOND_CHAR).toUpperCase() + first.substring(SECOND_CHAR, first.length()).toLowerCase()); // append first name with capital first letter and lower rest
+        fullName.append(" "); // append space
+        fullName.append(last.substring(FIRST_CHAR, SECOND_CHAR).toUpperCase() + last.substring(SECOND_CHAR, first.length()).toLowerCase()); // append last name with capital first letter and lower rest
+
+        return fullName.toString(); // return first and last name
     }
 
     /**
      * Method that will reverse a full name.
-     * @return reversed
+     *
+     * @return Full name reversed
      */
     public String getReverseName()
     {
+        // reversed name string builder initialization
         final StringBuilder reversedName;
-
         reversedName = new StringBuilder();
-        reversedName.append(StringUtils.reverse(first));
-        reversedName.append(" ");
-        reversedName.append(StringUtils.reverse(last));
 
-        return reversedName.toString();
+        reversedName.append(StringUtils.reverse(last)); // append last name
+        reversedName.append(" "); // append a space
+        reversedName.append(StringUtils.reverse(first)); // append first name
 
+        return reversedName.toString(); // return full reversed name
     }
-
 }
